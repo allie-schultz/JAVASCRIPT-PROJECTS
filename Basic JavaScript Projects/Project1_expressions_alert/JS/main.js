f4922a8d7549c2cd26486029f8f91d584842f85a
Allie Schultz <allie.m.schultz1@gmail.com>
2025-02-19 14:00:31 -0800

    added variable alert assignment

--- Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -1,3 +1,10 @@
-window.alert("Hello, World!")
+window.alert("Hello, World!");
+document.write("Hello, World!");
 
-document.write("Hello, World!")
+// variable assignment
+var A = "<br>This is a string";
+document.write(A);
+
+// variable to alert assignment
+var B = "This is a string in an alert message.";
+alert(B);
