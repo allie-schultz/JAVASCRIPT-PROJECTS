2b392425c78848bc00bf919ebbf2f0fb4f3fdce6
Allie Schultz <allie.m.schultz1@gmail.com>
2025-02-19 20:01:21 -0800

    created new folder and files for function assignment

---
+++ Basic JavaScript Projects/Project2_functions/JS/main.js
@@ -0,0 +1,5 @@
+// first function assignment
+function buttonText() {
+    var text = "I'm an awesome button!"; // creates string variable
+    document.getElementById("btn_text").innerHTML = text; // assigns variable value to HTML element with id "btn_text"
+}
