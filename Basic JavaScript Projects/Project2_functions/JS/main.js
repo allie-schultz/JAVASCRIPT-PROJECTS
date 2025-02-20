36eadd6df608c9033079241d5d35795bb419a9a0
Allie Schultz <allie.m.schultz1@gmail.com>
2025-02-19 20:15:09 -0800

    added opperand assignment

--- Basic JavaScript Projects/Project2_functions/JS/main.js
+++ Basic JavaScript Projects/Project2_functions/JS/main.js
@@ -3,3 +3,10 @@ function buttonText() {
     var text = "Why did you click it!!!"; // creates string variable
     document.getElementById("paragraph_text").innerHTML = text; // assigns variable value to HTML element with id "btn_text"
 }
+
+// concatenate with opperant assignment
+function buttonText2(){
+    var text2 = "Seriously? Again!?";
+    text2 += " I said no buttons!"; // concatenate with opperand
+    document.getElementById("paragraph_text").innerHTML = text2;
+}
