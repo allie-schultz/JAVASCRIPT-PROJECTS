5d5b72a434f0b143879b2f77208a12d5ee84ddbf
Allie Schultz <allie.m.schultz1@gmail.com>
2025-02-20 02:21:37 -0800

    added additional mathmatics assignments

--- Basic JavaScript Projects/Project3_math_operators/JS/main.js
+++ Basic JavaScript Projects/Project3_math_operators/JS/main.js
@@ -35,4 +35,60 @@ document.addEventListener('DOMContentLoaded', function() {
     divide();
 });
 
+// multiple opperation assignment
+function multiple_opperations(){
+    var answer = (25 + 10) / (5 * (7 - 3));
+    document.getElementById("opperations").innerHTML = "25 plus 10, divided by the product of the number 5 multiplied by the difference of 7 and 3, is equal to " + answer;
+}
+document.addEventListener('DOMContentLoaded', function() {
+    multiple_opperations();
+});
+
+// modulus opperator assignment
+function modulus(){
+    var remainder = 30 % 9;
+    document.getElementById("remain").innerHTML = "When you divide 30 by 9, you are left with a remainder of: " + remainder;
+}
+document.addEventListener('DOMContentLoaded', function() {
+    modulus();
+});
+
+// negation opperator assignment
+function negate(){
+    var num = 27;
+    document.getElementById("neg").innerHTML = -num;
+}
+document.addEventListener('DOMContentLoaded', function() {
+    negate();
+});
+
+// increment opperator assignment
+function increment(){
+    var x = 13
+    x++;
+    document.getElementById("increase").innerHTML = x;
+}
+document.addEventListener('DOMContentLoaded', function() {
+    increment();
+});
+
+// decrement opperator assignment
+function decrement(){
+    var x = 13
+    x--;
+    document.getElementById("decrease").innerHTML = x;
+}
+document.addEventListener('DOMContentLoaded', function() {
+    decrement();
+});
+
+// random number assignment
+var rand_num = (Math.random() * 50)
+document.getElementById("random").innerHTML = Math.trunc(rand_num);
+
+// rmath object achallenge
+var square_rt = (Math.sqrt(49))
+document.getElementById("sqr").innerHTML = square_rt;
+
+
 
